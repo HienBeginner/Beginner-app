4c1e4bedfd95d0583e50c00abd36ef59a2f26408
Peter <155692507+HienBeginner@users.noreply.github.com>
2024-01-04 18:19:09 -0800

    Add files via upload

---
+++ App/server.js
@@ -0,0 +1,62 @@
+
+const io  = require('socket.io')(3000, {
+    cors: {
+        origin:['http://localhost:8080']
+    }
+})
+const users = {}
+const rooms = {}
+io.on('connection',socket=>{
+    users[socket.id]=''
+    socket.on('name',name=>{
+        users[socket.id]=name
+    })
+    socket.on('check-id',id=>{
+        if(!(id in users)) {
+            io.to(socket.id).emit('no-id',id)
+        }
+        else {
+            io.to(socket.id).emit('foundID',id)
+        }
+    })
+    socket.on('send-message',(message,room)=>{
+        socket.to(room).emit('receive-message',{message:message,name:users[socket.id]})
+    })
+    socket.on('join-room',roomID=>{
+        let event = ''
+        if(!checkRoomID(roomID)) {
+            event= 'invalid-room'
+        }
+        else {
+            event = 'valid-room'
+            socket.join(roomID)
+            rooms[roomID]++
+            io.to(roomID).emit('update-online-users',rooms[roomID])
+            socket.to(roomID).emit('send-notifi-users',users[socket.id]+' joined in the conversation')
+        }
+        io.to(socket.id).emit(event,roomID)
+    })
+    socket.on('create-room',roomID=>{
+        let event =''
+        if(checkRoomID(roomID)) {
+            event = 'room-exist'
+        }
+        else {
+            event = 'room-available'
+            socket.join(roomID)
+            rooms[roomID] =1
+        }
+        io.to(socket.id).emit(event,roomID)
+
+    })
+    socket.on('leave-room',roomID=>{
+        socket.leave(roomID)
+        rooms[roomID]--
+        if(rooms[roomID]==0) delete rooms[roomID]
+        io.emit('update-online-users',rooms[roomID])
+        socket.to(roomID).emit('send-notifi-users',users[socket.id]+' left')
+    })
+})
+function checkRoomID(roomID) {
+    return roomID in rooms
+}
